6f34f76d8f3ea9689ab41ffc850e8856cfb85498
DavidArthurCole <davidarthurcole23@gmail.com>
2025-09-22 00:08:14 -0400

    Bruh

--- db/migrations/4_mission_add_mission_type.up.sql
+++ db/migrations/4_mission_add_mission_type.up.sql
@@ -3,7 +3,7 @@
 -- Any ship that exists in the database from before this update gets set to 0 (Standard).
 -- Otherwise default to -1, and let the app set it on first fetch.
 
-ALTER TABLE mission_info ADD COLUMN mission_type INTEGER NOT NULL DEFAULT -1;
+ALTER TABLE mission ADD COLUMN mission_type INTEGER NOT NULL DEFAULT -1;
 -- Leaving a window of ~12 hours before the update as the cutoff, 
 -- Wednesday, September 17, 2025 12:00:00 AM, --> 1758067200
-UPDATE mission_info SET mission_type = 0 WHERE start_timestamp < 1758067200;
+UPDATE mission SET mission_type = 0 WHERE start_timestamp < 1758067200;
